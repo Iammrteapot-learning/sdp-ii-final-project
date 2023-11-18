f3455cc773b19de6e234182696373e7353c35d08
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2023-11-18 20:09:10 +0700

    feat: :sparkles: auth page layout

--- src/app/user/(authentication)/auth/page.tsx
+++ src/app/user/(authentication)/auth/page.tsx
@@ -1,3 +1,39 @@
+'use client'
+import SignInForm from '@/components/Common/SignInForm/SignInForm'
+import SignUpForm from '@/components/Common/SignUpForm/SignUpForm'
+import { useState } from 'react'
+
 export default function UserAuthPage() {
-  return <div>USER AUTH PAGE</div>
+  const [isSignInFormDisplayed, setIsSignInFormDisplayed] = useState(true)
+
+  return (
+    <div className="flex p-[100px] justify-center items-center bg-orange-300 min-h-[101vh] overflow-hidden">
+      {isSignInFormDisplayed && (
+        <SignInForm
+          handleSignIn={function (email: string, password: string): void {
+            throw new Error('Function not implemented.')
+          }}
+          handleCreateNewAccount={() => setIsSignInFormDisplayed(false)}
+        />
+      )}
+      {!isSignInFormDisplayed && (
+        <SignUpForm
+          handleSignUp={function ({
+            username,
+            tel,
+            email,
+            password,
+          }: {
+            username: string
+            tel: string
+            email: string
+            password: String
+          }): void {
+            throw new Error('Function not implemented.')
+          }}
+          handleBackToSignIn={() => setIsSignInFormDisplayed(true)}
+        />
+      )}
+    </div>
+  )
 }
