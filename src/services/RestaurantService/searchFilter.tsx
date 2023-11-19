c81cf28abf329d4ef00ed3045b6c1dbee88f378f
Tinnaung <111551053+Tinnaung@users.noreply.github.com>
2023-11-20 00:22:24 +0700

    [GGK-11] Make search functional (#39)

---
+++ src/services/RestaurantService/searchFilter.tsx
@@ -0,0 +1,21 @@
+export default function searchFilter({ res_list, searchWord }: { res_list: any[], searchWord: string }) {
+  // Check if res_list is defined and is an array
+  if (!Array.isArray(res_list)) {
+    console.error('res_list is not defined or not an array');
+    return [];
+  }
+
+  if (searchWord === "") {
+    return res_list;
+  }
+
+  const lowercasedSearchWord = searchWord.toLowerCase(); // Convert search word to lowercase
+
+  const result = res_list.filter((res) =>
+    res.name.toLowerCase().includes(lowercasedSearchWord) ||
+    res.province.toLowerCase().includes(lowercasedSearchWord) ||
+    res.foodType.toLowerCase().includes(lowercasedSearchWord)
+  );
+
+  return result;
+}
