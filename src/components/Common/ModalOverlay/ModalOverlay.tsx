9233a0e29fc05bbd53d07ef93b2c10df16dc48ec
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2023-11-20 04:35:59 +0700

    fix: :bug: modal overlay

--- src/components/Common/ModalOverlay/ModalOverlay.tsx
+++ src/components/Common/ModalOverlay/ModalOverlay.tsx
@@ -4,28 +4,23 @@ import React from 'react'
 export default function ModalOverlay({
   isVisible,
   onClose = () => {},
-  zIndex = 20,
   children,
 }: {
   isVisible: boolean
   onClose: () => void
-  zIndex?: number
   children?: React.ReactNode
 }) {
   return (
     <div
-      className={`fixed top-0 left-0 w-[100%] h-[100vh] justify-center items-center ${
+      className={`fixed top-[24px] left-0 w-[100%] h-[100vh] justify-center items-center ${
         isVisible ? 'flex' : 'hidden'
       }`}
     >
       <div
-        className={`bg-black w-[100%] h-[100vh] opacity-20 absolute top-0 left-0 z-[${zIndex}]`}
+        className={`bg-black w-[100%] h-[100vh] opacity-20 absolute top-0 left-0 z-[90]`}
         onClick={onClose}
       />
-      <div
-        className={`z-[${zIndex + 10}] relative`}
-        onClick={(e) => e.stopPropagation()}
-      >
+      <div className={`z-[100] relative`} onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
     </div>
