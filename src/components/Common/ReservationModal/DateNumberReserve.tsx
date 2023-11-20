648717cfb4993bd96afd9916d0166d79d33a6d11
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2023-11-20 22:35:17 +0700

    [GGK-52] Fix Reservation Modal (#50)

--- src/components/Common/ReservationModal/DateNumberReserve.tsx
+++ src/components/Common/ReservationModal/DateNumberReserve.tsx
@@ -8,6 +8,7 @@ import TextField from '@mui/material/TextField'
 import { DateValidationError } from '@mui/x-date-pickers/models'
 
 import { Unstable_NumberInput as NumberInput } from '@mui/base/Unstable_NumberInput'
+import NumberOfGuestIcon from '../Icon/NumberOfGuestIcon'
 
 export default function DateNumberReserve({
   onDateChange,
@@ -15,95 +16,88 @@ export default function DateNumberReserve({
   isError,
   setupNumber,
   setupDate,
-  isReset
+  isReset,
 }: {
   onDateChange: Function
   onNumberChange: Function
   isError: boolean
-  setupNumber:number
+  setupNumber: number
   setupDate: Dayjs | null
-  isReset:boolean
+  isReset: boolean
 }) {
   const [reserveDate, setReserveDate] = useState<Dayjs | null>(null)
   const [reserveNumber, setReserveNumber] = useState<number>(1)
   useEffect(() => {
     setReserveNumber(setupNumber)
-  },[setupNumber])
+  }, [setupNumber])
   useEffect(() => {
     setReserveDate(setupDate)
-  },[setupDate])
+  }, [setupDate])
   useEffect(() => {
     setReserveNumber(setupNumber)
     setReserveDate(setupDate)
-  },[isReset])
+  }, [isReset])
 
   return (
-    <div className="w-[500px] h-[130px] relative ">
-      <div className="w-[500px] h-40 left-0 absolute opacity-50 bg-zinc-200 rounded-[30px]" />
-      <div className="w-[439px] h-auto left-[63px] top-[11px] absolute flex flex-row justify-center space-x-32 ">
-        <div className="left-0 top-[10px] absolute text-black text-base font-normal font-['Helvetica Neue'] leading-normal">
-          Reservation Date
+    <div className="w-[500px] p-4">
+      <div className="w-full bg-gray-200 rounded-[30px] flex flex-col gap-4 py-4">
+        <div className="flex flex-row items-center">
+          <div className="w-[40%] text-black text-base text-right tracking-wider pe-4 font-normal font-['Helvetica-Neue'] leading-normal">
+            Reservation Date
+          </div>
+          <div className="flex flex-col">
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                // className="bg-white"
+                className={
+                  isError
+                    ? 'rounded ring-offset-1 ring-1 ring-red-500 bg-white'
+                    : 'bg-white'
+                }
+                value={reserveDate}
+                disablePast={true}
+                onChange={(value) => {
+                  setReserveDate(value)
+                  onDateChange(value)
+                }}
+              />
+              {isError ? (
+                <div className="text-xs text-red-500 mt-1">
+                  * Please Enter Your Reservation Date
+                </div>
+              ) : null}
+            </LocalizationProvider>
+          </div>
         </div>
-        <div className="flex flex-col">
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-              // className="bg-white"
-              className={
-                isError
-                  ? 'rounded ring-offset-1 ring-1 ring-red-500 bg-white'
-                  : 'bg-white'
-              }
-              value={reserveDate}
-              disablePast={true}
-              onChange={(value) => {
-                setReserveDate(value)
-                onDateChange(value)
+        <div className="flex flex-row items-center">
+          <div className="w-[40%] pe-4 text-right items-center flex gap-2 justify-end">
+            <NumberOfGuestIcon />
+            <div className="text-black text-base font-normal font-['Helvetica-Neue'] leading-normal">
+              Number of Guests
+            </div>
+          </div>
+          <div className="flex h-max items-center justify-start">
+            <TextField
+              id="outlined-number"
+              label="Number"
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              inputProps={{ min: 1, max: 100, steps: 1 }}
+              value={reserveNumber}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                const inputValue = event.target.value
+                const numericValue = parseInt(inputValue, 10)
+
+                if (!isNaN(numericValue)) {
+                  setReserveNumber(numericValue)
+                  onNumberChange(numericValue)
+                }
               }}
             />
-            {isError ? <div className='text-xs text-red-500 mt-1'>* Please Enter Your Reservation Date</div>:null}
-          </LocalizationProvider>
+          </div>
         </div>
-      </div>
-      <div className="w-auto h-auto left-[31px] top-[80px] absolute flex flex-row justify-start">
-        <svg
-          width="14"
-          height="15"
-          viewBox="0 0 14 15"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="mt-8"
-        >
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M7 6C7.79565 6 8.55871 5.68393 9.12132 5.12132C9.68393 4.55871 10 3.79565 10 3C10 2.20435 9.68393 1.44129 9.12132 0.87868C8.55871 0.316071 7.79565 0 7 0C6.20435 0 5.44129 0.316071 4.87868 0.87868C4.31607 1.44129 4 2.20435 4 3C4 3.79565 4.31607 4.55871 4.87868 5.12132C5.44129 5.68393 6.20435 6 7 6ZM0 15C-1.36979e-08 14.0807 0.18106 13.1705 0.532843 12.3212C0.884626 11.4719 1.40024 10.7003 2.05025 10.0503C2.70026 9.40024 3.47194 8.88463 4.32122 8.53284C5.1705 8.18106 6.08075 8 7 8C7.91925 8 8.8295 8.18106 9.67878 8.53284C10.5281 8.88463 11.2997 9.40024 11.9497 10.0503C12.5998 10.7003 13.1154 11.4719 13.4672 12.3212C13.8189 13.1705 14 14.0807 14 15H0Z"
-            fill="#38BDF8"
-          />
-        </svg>
-
-        <div className="left-0 mt-8  ml-3 top-[10px] text-black text-base font-normal font-['Helvetica Neue'] leading-normal">
-          Number of Guests
-        </div>
-        <TextField
-          id="outlined-number"
-          label="Number"
-          type="number"
-          className="mt-4 ml-10"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          inputProps={{ min: 1, max: 100, steps: 1 }}
-          value={reserveNumber}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            const inputValue = event.target.value
-            const numericValue = parseInt(inputValue, 10)
-
-            if (!isNaN(numericValue)) {
-              setReserveNumber(numericValue)
-              onNumberChange(numericValue)
-            }
-          }}
-        />
       </div>
     </div>
   )
--- src/components/Common/ReservationModal/ReservationModal.tsx
+++ src/components/Common/ReservationModal/ReservationModal.tsx
@@ -13,51 +13,53 @@ type ReservationModalProps = {
   reserve_date?: Dayjs
   reserve_number?: number
   isVisible: boolean
-  onClose_Confirm: () => void
-  onClose_Cancel: () => void
+  onCloseConfirm: () => void
+  onCloseCancel: () => void
   onDateNumberChange: (date: Dayjs | null, number: number) => void
 }
 
-export default function ReservationModal(param: ReservationModalProps) {
-  var initNumber = 1
-  var initDate = null
-  if (!!param.reserve_number) {
-    initNumber = param.reserve_number
-  }
-  if (!!param.reserve_date) {
-    initDate = param.reserve_date
-  }
-  const [bookingDate, setBookingDate] = useState<Dayjs | null>(null)
+export default function ReservationModal(props: ReservationModalProps) {
+  const {
+    name,
+    address,
+    tel,
+    isVisible,
+    onCloseConfirm,
+    onCloseCancel,
+    onDateNumberChange,
+  } = props
+
+  const [bookingDate, setBookingDate] = useState<Dayjs>(dayjs())
   const [guestNumber, setGuestNumber] = useState<number>(1)
   const [error, setError] = useState(false)
-  const errorMessage = 'Please fill in your reservation date'
-  //mock user session data
-  const username = 'Thanakrit Yingwatthanakul'
-  const tel_user = '081-234-5678'
+
   //remain POST data & Redux at onClick button
   useEffect(() => {
-    setGuestNumber(initNumber)
-    setBookingDate(initDate)
-  },[param.isVisible])
+    setGuestNumber(0)
+    setBookingDate(dayjs())
+  }, [props.isVisible])
 
   return (
-    <ModalOverlay isVisible={param.isVisible} onClose={param.onClose_Cancel}>
-      <div className="w-[680px] relative bg-zinc-100 rounded-[30px] shadow flex-col justify-start items-center inline-flex py-4">
+    <ModalOverlay isVisible={isVisible} onClose={onCloseCancel}>
+      <div
+        className="w-[680px] max-h-[450px] overflow-auto no-scrollbar bg-zinc-100 rounded-[30px] shadow 
+      flex-col justify-start items-center flex py-6"
+      >
         <div className="flex-col justify-center items-center text-center">
           <div className="px-3 w-fit bg-red-500 rounded-[20px] shadow-inner">
-            <div className="text-center text-white text-4xl font-medium font-['Helvetica Neue'] leading-[72px]">
-              {param.name}
+            <div className="text-center text-white text-4xl font-medium font-['Helvetica-Neue'] leading-[72px]">
+              {name}
             </div>
           </div>
-          <div className="relative text-center text-red-500 text-4xl font-bold font-['Helvetica Neue'] underline leading-[72px]">
+          <div className="mb-6 text-center text-red-500 text-4xl font-bold font-['Helvetica-Neue'] underline leading-[72px]">
             Reservation
           </div>
         </div>
-        <InfoPanel location={param.address} tel={param.tel} />
-        <ShowUserInfoPanel username={username} tel={tel_user} />
+        <InfoPanel location={address} tel={tel} />
+        <ShowUserInfoPanel username={name} tel={tel} />
         <DateNumberReserve
-          setupNumber={initNumber}
-          setupDate={initDate}
+          setupNumber={1}
+          setupDate={dayjs()}
           onDateChange={(value: Dayjs) => {
             setError(false)
             setBookingDate(value)
@@ -66,17 +68,17 @@ export default function ReservationModal(param: ReservationModalProps) {
             setGuestNumber(value)
           }}
           isError={error}
-          isReset={param.isVisible}
+          isReset={isVisible}
         />
-        <div className="w-[230px] h-10 mt-10 relative space-x-10">
+        <div className="mt-8 flex gap-8">
           <button
-            className="px-4 py-2 bg-sky-400 rounded justify-start items-center gap-2 inline-flex text-white text-base font-medium"
+            className="px-4 py-2 bg-sky-400 hover:bg-sky-500 rounded justify-start items-center gap-2 inline-flex text-white text-base font-medium"
             onClick={() => {
               if (!!bookingDate) {
-                if (param.onDateNumberChange!!) {
-                  param.onDateNumberChange(bookingDate, guestNumber)
+                if (onDateNumberChange!!) {
+                  onDateNumberChange(bookingDate, guestNumber)
                 }
-                param.onClose_Confirm()
+                onCloseConfirm()
               } else {
                 setError(true)
               }
@@ -85,12 +87,12 @@ export default function ReservationModal(param: ReservationModalProps) {
             Confirm
           </button>
           <button
-            className="px-4 py-2 bg-red-500 rounded justify-start items-center gap-2 inline-flex text-white text-base font-medium"
+            className="px-4 py-2 bg-red-500 hover:bg-red-600 rounded justify-start items-center gap-2 inline-flex text-white text-base font-medium"
             onClick={() => {
               setError(false)
-              setGuestNumber(initNumber)
-              setBookingDate(initDate)
-              param.onClose_Cancel()
+              setGuestNumber(1)
+              setBookingDate(dayjs())
+              onCloseCancel()
             }}
           >
             Cancel
