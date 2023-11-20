e20d219127b43503350deac48020ef1bfb6c4567
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2023-11-20 22:34:28 +0700

    fix: :bug: fix css

--- src/components/Common/ReservationModal/ShowUserInfoPanel.tsx
+++ src/components/Common/ReservationModal/ShowUserInfoPanel.tsx
@@ -6,23 +6,9 @@ export default function ShowUserInfoPanel({
   tel: string
 }) {
   return (
-    <div className="w-[359px] h-[97px] relative mt-12 flex flex-col items-center justify-start">
-      <div className="w-[359px] absolute inline-flex justify-center space-x-28">
-        <div className="left-0 top-[10px] absolute text-black text-base font-normal font-['Helvetica Neue'] leading-normal">
-          Name :
-        </div>
-        <div className="left-0 top-[10px] absolute text-black text-base font-normal font-['Helvetica Neue'] leading-normal">
-          {username}
-        </div>
-      </div>
-      <div className="w-[359px] left-[23px] top-[36px] absolute inline-flex justify-center space-x-28">
-        <div className="left-0 top-[10px] absolute text-black text-base font-normal font-['Helvetica Neue'] leading-normal">
-          Tel :
-        </div>
-        <div className="left-0 top-[10px]  absolute text-black text-base font-normal font-['Helvetica Neue'] leading-normal">
-          {tel}
-        </div>
-      </div>
+    <div className="w-full my-6 flex flex-col items-center justify-center gap-4">
+      <div>Name : {username}</div>
+      <div>Tel : {tel}</div>
     </div>
   )
 }
