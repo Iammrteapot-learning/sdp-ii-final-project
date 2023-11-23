42e7bf8480a86f6f519784063bbbf63846dcad74
Tinnaung <6430129921@student.chula.ac.th>
2023-11-23 13:52:16 +0700

    [GGK-1007] Add Hover

--- src/components/Common/BackButton/BackButton.tsx
+++ src/components/Common/BackButton/BackButton.tsx
@@ -5,7 +5,7 @@ export default function BackButton() {
     <div
       className="flex items-center py-2 px-4 rounded-full 
         bg-red-500 text-white text-center font-medium leading-[150%]
-        w-[fit-content] h-[fit-content]
+        w-[fit-content] h-[fit-content] hover:bg-red-600
       "
       role="button"
       onClick={() => window.history.back()}
--- src/components/Common/Icon/EditIcon.tsx
+++ src/components/Common/Icon/EditIcon.tsx
@@ -1,6 +1,7 @@
 export default function EditIcon() {
   return (
     <svg
+    className="hover:rounded-full hover:bg-sky-200"
       xmlns="http://www.w3.org/2000/svg"
       width="40"
       height="40"
